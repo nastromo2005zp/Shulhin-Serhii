c19f2aa492d571ab8bb23e445268691fdb768f3e
nastromo77 <nastromo2005@ukr.net>
2022-09-15 11:19:41 +0300

    stage 4

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
-    $(document).ready(function() {
+   // $(document).ready(function() {
 
 
-        const $j = 2; //amount of rows activities to select
-        var $m = 0; //variable for customize backgrounds of selectors elements through consctruction switch case
+        const j = 2; //amount of rows activities to select
+        var m = 0; //variable for customize backgrounds of selectors elements through consctruction switch case
 
-        for ($i = 0; $i < $j; $i++) {
+        for (i = 0; i < j; i++) {
             //define main div which contains options of activities
             const activitySelector = document.querySelector('#activitySelector');
             //define div which contains row with 2 options of activities
@@ -13,7 +13,7 @@
             //set required attributes to the div
             divRow.setAttribute('class', 'row menu d-flex justify-content-around');
             //filling 2 options of activities and put them to htmlDOM
-            for ($k = 0; $k < 2; $k++) {
+            for (k = 0; k < 2; k++) {
 
                 const responceFromServer = httpGet('http://www.boredapi.com/api/activity/');//making a request to the server
 
@@ -24,33 +24,36 @@
 
                 //selecting backgrounds
 
-                if ($m >= 4) $m -= 4; //reset count to select of 4 colors, in case amount of rows activities to select is > 2
-                switch ($m) {
+                if (m >= 4) m -= 4; //reset count to select of 4 colors, in case amount of rows activities to select is > 2
+
+                switch (m) {
                     case 0:
-                        divCols.setAttribute('class', 'col-6 col-sm-6 menu__menuItem menu__menuItem_first');
-                        $m++;
+                        divCols.setAttribute('class', 'col-6 col-sm-6 menu menu__menuItem menu__menuItem_first');
+                        m++;
                         break;
 
                     case 1:
-                        divCols.setAttribute('class', 'col-6 col-sm-6 menu__menuItem menu__menuItem_second');
-                        $m++;
+                        divCols.setAttribute('class', 'col-6 col-sm-6 menu menu__menuItem menu__menuItem_second');
+                        m++;
                         break;
 
                     case 2:
-                        divCols.setAttribute('class', 'col-6 col-sm-6 menu__menuItem menu__menuItem_third');
-                        $m++;
+                        divCols.setAttribute('class', 'col-6 col-sm-6 menu menu__menuItem menu__menuItem_third');
+                        m++;
                         break;
 
                     case 3:
-                        divCols.setAttribute('class', 'col-6 col-sm-6 menu__menuItem menu__menuItem_fourth');
-                        $m++;
+                        divCols.setAttribute('class', 'col-6 col-sm-6 menu menu__menuItem menu__menuItem_fourth');
+                        m++;
                         break;
 
                     default:
-                        divCols.setAttribute('class', 'col-6 col-sm-6 menu__menuItem menu__menuItem_first');
-                        $m++;
+                        divCols.setAttribute('class', 'col-6 col-sm-6 menu menu__menuItem menu__menuItem_first');
+                        m++;
                         break;
                 }
+
+
 
                 //creating card of activities
                 const titleOfActivity = document.createElement('p');
@@ -78,47 +81,80 @@
 
 
         $('div.menu__menuItem').on('click', function(e) {
-            let $activity = {
+            let activity = {
                 "title" : "",
-                "type" : "",
+                "category" : "",
+                "classTitle" : "",
             };
-            $activity = getActivities();
-            //console.log($activity);
+            activity = getActivities();
+
+
+
             
 
             function getActivities() {
                 //in case user click on 'P'
                 if (e.target.tagName === 'P') {
-                    $activity.title = e.target.innerText;
 
-                    $activity.type = e.target.nextSibling.innerText;
+                    activity.title = e.target.innerText;
 
-                    return $activity;
+                    activity.category = e.target.nextSibling.innerText;
+
+                    activity.classTitle = e.target.parentNode.getAttribute('class');
+
+                    return activity;
 
                 }
                 //in case user click on 'H4'
                 else if (e.target.tagName === 'H4') {
-                    $activity.title = e.target.previousSibling.innerText;
-                    $activity.type = e.target.innerText;
-                    return $activity;
+                    activity.title = e.target.previousSibling.innerText;
+                    activity.category = e.target.innerText;
+                    activity.classTitle = e.target.parentNode.getAttribute('class');
+                    return activity;
 
                 }
-
-                    $activity.title = e.target.querySelector('p').innerText;
-                    $activity.type = e.target.querySelector('h4').innerText;
-                    return $activity;
+                //in case user click on 'DIV'
+                    activity.title = e.target.querySelector('p').innerText;
+                    activity.category = e.target.querySelector('h4').innerText;
+                    activity.classTitle = e.target.getAttribute('class');
+                    return activity;
 
 
 
 
             }
+            $('.add-remove').slick('slickAdd', '<div class="slide d-flex justify-content-center align-items-center"><div class="' + activity.classTitle + '"><p>' + activity.title + '</p><h4>' + activity.category + '</h4></div>');
+            console.log(activity.title, activity.category, activity.classTitle);
+
+        });
+
+       //});
 
 
+        //slick-slider code
+        $('.add-remove').slick({
+            dots: true,
 
         });
 
 
 
 
+        $(document).delegate( ".slide", "click", function() {
+            let curSlide = $('.add-remove').slick('slickCurrentSlide');
+            $(".add-remove").slick('slickRemove', curSlide, false).slick('refresh');
+
+
+
+
+
+
+
+
+
+
+
     });
+
+        
 
